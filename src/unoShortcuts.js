c076936704a6db6d45349c5f492e9168ef0a3b2c
Sara Tapusoa <79812543+stapusoa@users.noreply.github.com>
2024-05-12 13:55:32 -0600

    adjusted unocss config

---
+++ src/unoShortcuts.js
@@ -0,0 +1,9 @@
+export const unoShortcuts = {
+    'figma-plugin-styles': 'm-0 font-inter font-not-oblique tracking-normal leading-tight',
+    'type-pg-heading': 'figma-plugin-styles text-stone-50 text-3.5 font-semibold',
+    'type-pg-description': 'figma-plugin-styles text-stone-200 text-2.5 font-regular',
+    'type-pg-action': 'figma-plugin-styles text-stone-50 text-2.5 font-medium',
+    'type-pg-label': 'figma-plugin-styles text-zinc-300 text-2.5 font-medium',
+    'type-pg-tab': 'figma-plugin-styles text-neutral-400 text-2.5 font-semibold',
+    'type-pg-tab-active': 'figma-plugin-styles text-stone-50 text-2.5 font-semibold',
+};
---
+++ src/unoTheme.js
@@ -0,0 +1,26 @@
+export const unoTheme = {
+    colors: {
+        green: '#13AE5C',
+        charcoal: '#2C2C2C',
+        stone: {
+            50: '#fafaf9',
+            200: '#e7e5e4',
+        },
+        neutral: {
+            400: '#a3a3a3',
+        },
+        zinc: {
+            300: '#d4d4d8',
+        },
+    },
+    fontFamily: {
+        archivo: '"Archivo Narrow", sans-serif',
+        roboto: '"Roboto", sans-serif',
+        inter: '"Inter", sans-serif',
+    },
+    boxShadow: {
+        sm: '0 1px 3px rgba(0, 0, 0, 0.1)',
+        md: '0 3px 12px rgba(0, 0, 0, 0.08)',
+        lg: '0 10px 20px rgba(0, 0, 0, 0.1)',
+    },
+};
