46806dc446868e0dbfc3bc19604231859bc9e6f3
Sara Tapusoa <79812543+stapusoa@users.noreply.github.com>
2024-05-15 12:32:01 -0600

    styled the select icon using mui icon

---
+++ src/custom-types.d.ts
@@ -0,0 +1,10 @@
+declare module 'unoTheme' {
+  export const unoTheme: {
+    fontFamily: {
+      archivo: string,
+      roboto: string,
+      inter: string,
+      material: string[]
+    }
+  };
+}
